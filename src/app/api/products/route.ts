c13eb4830719145aefbf147bd23ebbda980887b7
Lloyd Boone <lboone@progressive.com>
2024-05-08 00:19:56 -0400

    implementing custom slider

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,10 +1,50 @@
 import { db } from "@/db"
+import { ProductFilterValidator } from "@/lib/validators/product-validator"
+import { NextRequest } from "next/server"
 
-export const POST = async () => {
+class Filter {
+    private filters: Map<string, string[]> = new Map()
+
+    hasFilters() {
+        return this.filters.size > 0
+    }
+    add(key: string, operator: string, value: string | number) {
+        const filter = this.filters.get(key) || []
+
+        filter.push(`${key} ${operator} ${typeof value === "number" ? value : `"${value}"`}`)
+        this.filters.set(key, filter)
+
+    }
+
+    addRaw(key: string, rawFilter: string) {
+        this.filters.set(key, [rawFilter])
+    }
+
+    get() {
+        const parts: string[] = []
+        this.filters.forEach((filter, key) => {
+        const groupedValues = filter.join(' OR ')
+        parts.push(`(${groupedValues})`)
+        })
+        return parts.join(' AND ')
+    }
+}
+
+export const POST = async (req: NextRequest) => {
+    const body = await req.json()
+
+    const {color, price, size, sort} = ProductFilterValidator.parse(body.filter)
+
+    const filter = new Filter()
+    color.forEach((color) => filter.add('color', '=', color))
+    color.forEach((size) => filter.add('size', '=', size))
+    filter.addRaw('price',`price >= ${price[0]} AND price <= ${price[1]}`)
+
     const products = await db.query({
         topK: 12,
         vector: [0, 0, 0],
         includeMetadata: true,
+        filter: filter.hasFilters() ? filter.get() : undefined,
     })
 
     return new Response(JSON.stringify(products))
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,6 +29,7 @@ import {
 import { AccordionContent } from "@radix-ui/react-accordion";
 import { ProductState } from "../../types";
 import { DEFAULT_CUSTOM_PRICE } from "@/consts";
+import { Slider } from "@/components/ui/slider";
 
 export default function Home() {
   const [filter, setFilter] = useState<ProductState>({
@@ -46,6 +47,9 @@ export default function Home() {
         {
           filter: {
             sort: filter.sort,
+            color: filter.color,
+            size: filter.size,
+            price: filter.price,
           },
         }
       );
@@ -74,7 +78,9 @@ export default function Home() {
     }
   };
 
-  console.log(filter);
+  const minPrice = Math.min(filter.price.range[0], filter.price.range[1]);
+  const maxPrice = Math.max(filter.price.range[0], filter.price.range[1]);
+
   return (
     <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
@@ -216,12 +222,14 @@ export default function Home() {
                               ...prev,
                               price: {
                                 isCustom: false,
-                                range: [...option.value]
+                                range: [...option.value],
                               },
-                            }))
+                            }));
                           }}
                           checked={
-                            !filter.price.isCustom && filter.price.range[0] === option.value[0] && filter.price.range[1] === option.value[1]
+                            !filter.price.isCustom &&
+                            filter.price.range[0] === option.value[0] &&
+                            filter.price.range[1] === option.value[1]
                           }
                           className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
                         />
@@ -233,8 +241,9 @@ export default function Home() {
                         </label>
                       </li>
                     ))}
-                    <li className="flex items-center">
-                    <input
+                    <li className="flex justify-center flex-col gap-2">
+                      <div>
+                        <input
                           type="radio"
                           id={`price-${PRICE_FILTERS.options.length}`}
                           onChange={() => {
@@ -242,13 +251,11 @@ export default function Home() {
                               ...prev,
                               price: {
                                 isCustom: true,
-                                range: [0, 100]
+                                range: [0, 100],
                               },
-                            }))
+                            }));
                           }}
-                          checked={
-                            filter.price.isCustom
-                          }
+                          checked={filter.price.isCustom}
                           className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
                         />
                         <label
@@ -257,9 +264,41 @@ export default function Home() {
                         >
                           Custom
                         </label>
+                      </div>
+                      <div className="flex justify-between">
+                        <p className="font-medium">Price</p>
                         <div>
-                          
+                          {filter.price.isCustom
+                            ? minPrice.toFixed(0)
+                            : filter.price.range[0].toFixed(0)}{" "}
+                          $ -{" "}
+                          {filter.price.isCustom
+                            ? maxPrice.toFixed(0)
+                            : filter.price.range[1].toFixed(0)}{" "}
+                          $
                         </div>
+                      </div>
+
+                      <Slider
+                        className={cn({ "opacity-50": !filter.price.isCustom })}
+                        disabled={!filter.price.isCustom}
+                        onValueChange={(range) => {
+                          const [newMin, newMax] = range;
+                          setFilter((prev) => ({
+                            ...prev,
+                            price: {
+                              isCustom: true,
+                              range: [newMin, newMax],
+                            },
+                          }));
+                        }}
+                        value={filter.price.isCustom ? filter.price.range : DEFAULT_CUSTOM_PRICE}
+                        min={DEFAULT_CUSTOM_PRICE[0]}
+                        defaultValue={DEFAULT_CUSTOM_PRICE}
+                        max={DEFAULT_CUSTOM_PRICE[1]}
+                        step={5}
+
+                      />
                     </li>
                   </ul>
                 </AccordionContent>
